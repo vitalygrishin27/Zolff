f66189e4edc406fb4652a19c40cf9698269fdd71
Gryshyn Vitaliy <vitaliy.gryshyn@extern.a1.at>
2020-07-12 14:35:31 +0300

    init1

--- src/main/java/app/DERFF.java
+++ src/main/java/app/DERFF.java
@@ -8,11 +8,6 @@ import java.util.Locale;
 
 @SpringBootApplication
 public class DERFF {
-    @Bean
-    public MultipartConfigElement multipartConfigElement() {
-        MultipartConfigFactory factory = new MultipartConfigFactory();
-        return factory.createMultipartConfig();
-    }
 
     public static void main(String[] args) {
    //     ApplicationContext context=new AnnotationConfigApplicationContext(Context.class);
